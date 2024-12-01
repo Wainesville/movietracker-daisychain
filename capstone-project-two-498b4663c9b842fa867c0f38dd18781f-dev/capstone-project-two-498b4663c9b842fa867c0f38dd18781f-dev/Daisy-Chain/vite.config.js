276b36a01c1b6f03da4ddf9dd0a3722b9cefc3cf
wainesville <wainesville@gmail.com>
2024-12-01 14:56:27 -0800

    deployments steps

--- capstone-project-two-498b4663c9b842fa867c0f38dd18781f-dev/capstone-project-two-498b4663c9b842fa867c0f38dd18781f-dev/Daisy-Chain/vite.config.js
+++ capstone-project-two-498b4663c9b842fa867c0f38dd18781f-dev/capstone-project-two-498b4663c9b842fa867c0f38dd18781f-dev/Daisy-Chain/vite.config.js
@@ -4,13 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Backend server URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') // Optional: remove "/api" from the forwarded path
-      },
-    },
+    port: 3000,
   },
 })
