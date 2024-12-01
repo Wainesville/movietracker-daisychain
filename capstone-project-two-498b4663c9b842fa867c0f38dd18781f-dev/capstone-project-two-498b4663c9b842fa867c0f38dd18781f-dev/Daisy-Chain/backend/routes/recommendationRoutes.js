b25db134c01856ab287616365688bb41902bedca
wainesville <wainesville@gmail.com>
2024-12-01 10:17:01 -0800

    inital commit

--- capstone-project-two-498b4663c9b842fa867c0f38dd18781f-dev/capstone-project-two-498b4663c9b842fa867c0f38dd18781f-dev/Daisy-Chain/backend/routes/recommendationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authenticate = require('../middleware/authenticate');
-const { addRecommendation, removeRecommendation } = require('../controllers/recommendationController');
-
-router.post('/add', authenticate, addRecommendation);
-router.post('/remove', authenticate, removeRecommendation);
-
-module.exports = router;
